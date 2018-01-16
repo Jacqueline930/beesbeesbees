5f4162bc4bb81fb09b8bc2be391a7267a0924285
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-01-15 18:48:01 -0600

    worked on HoneyMakerBee and ForageBee functions

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,12 @@
 var RetiredForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "find pollen";
+  this.canFly = true;
+  this.treasureChest = [];
 };
+RetiredForagerBee.prototype = Object.create(Bee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = function() {treasureChest ++};
 
+};
